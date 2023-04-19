0385ed32137f6df41c58b4133203bcba97555a09
Sergiy Baskov <master.flock@gmail.com>
2023-04-19 15:01:10 +0300

    User task

---
+++ m3-lambda/src/main/java/com/cydeo/UserTask/UserTest.java
@@ -0,0 +1,12 @@
+package com.cydeo.UserTask;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserTest {
+    public static void main(String[] args) {
+
+
+        List<User> users = new ArrayList<>();
+    }
+}
