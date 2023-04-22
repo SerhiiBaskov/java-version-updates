8e9cdba2868d1ce77e1709103efe1687caf87337
Sergiy Baskov <master.flock@gmail.com>
2023-04-22 18:23:56 +0300

    Double Colon Operators

---
+++ m4-double_colon_operators/src/main/java/com/cydeo/Calculator.java
@@ -0,0 +1,12 @@
+package com.cydeo;
+
+public class Calculator {
+
+    public static void  findSun (int x, int y) {
+        System.out.println("Sum = " +(x+y));
+    }
+
+    public void findMultiply(int x, int y){
+        System.out.println("Multiply = " +x*y);
+    }
+}
--- m4-double_colon_operators/src/main/java/com/cydeo/Main.java
+++ m4-double_colon_operators/src/main/java/com/cydeo/Main.java
@@ -1,8 +1,34 @@
 package com.cydeo;
 
+import java.util.function.*;
+
 public class Main {
     public static void main(String[] args) {
 
+         Predicate<Integer> pred = p -> p % 2 == 0;
+
+        Calculate sum = (x, y) -> System.out.println("Sum = " +(x+y));
+        Calculate s1 = (x, y) -> Calculator.findSun(x, y);
+
+        // Reference to a Static Method
+        Calculate s2 = Calculator::findSun;
+        s2.calculate(10, 20);
+
+
+        // Reference to an Instance Method
+        Calculator obj = new Calculator();
+        Calculate m1 = obj::findMultiply;
+        // or
+        Calculate m2 = new Calculator()::findMultiply;
+
+
+        BiFunction<String, Integer, String> func = (str, i) -> str.substring(i);
+
+        BiFunction<String, Integer, String> func1 = String::substring;
+
+
+        Function<Integer, Double> b = new MyClass() :: method;
+        BiFunction<MyClass, Integer, Double> b1 = MyClass::method;
 
 
     }
