f5ffa48d36562a56bf4f021413eaa31e81cc2cbb
Sergiy Baskov <master.flock@gmail.com>
2023-04-23 14:01:46 +0300

    Comparator

---
+++ m5-comparator_interface/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,25 @@
+package com.cydeo;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(10, 0, 15, 5, 20);
+        System.out.println(list);
+
+        // Ascending
+        Collections.sort(list);
+        System.out.println(list);
+
+        // Descending
+        Collections.sort(list, new MyComparator());
+        System.out.println(list);
+
+    }
+
+
+}
