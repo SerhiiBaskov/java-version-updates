95d5959372e730b5d6b1b3003a9cec6edb127fb2
Sergiy Baskov <master.flock@gmail.com>
2023-04-28 14:36:10 +0300

    Stream

---
+++ m6-stream/src/main/java/com/cydeo/Course.java
@@ -0,0 +1,12 @@
+package com.cydeo;
+
+public class Course {
+
+    private String className;
+    private int courseID;
+
+    public Course(String className, int courseID) {
+        this.className = className;
+        this.courseID = courseID;
+    }
+}
