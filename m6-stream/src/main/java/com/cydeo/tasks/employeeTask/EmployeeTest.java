75a2a2a8d65fb884ddc0a23392afff829ea47d11
Sergiy Baskov <master.flock@gmail.com>
2023-05-27 21:29:41 +0300

    Finding & Matching

--- m6-stream/src/main/java/com/cydeo/tasks/employeeTask/EmployeeTest.java
+++ m6-stream/src/main/java/com/cydeo/tasks/employeeTask/EmployeeTest.java
@@ -1,15 +1,30 @@
 package com.cydeo.tasks.employeeTask;
 
+import java.util.Collection;
+import java.util.List;
+
 public class EmployeeTest {
     public static void main(String[] args) {
 
-        // Print all emails
+        System.out.println("Print all emails");
 
         EmployeeData.readAll()
                 .map(Employee::getEmpEmail)
                 .forEach(System.out::println);
 
+        System.out.println("Print all phone numbers");
 
+        EmployeeData.readAll()
+//                .map(Employee::getEmpPhoneNumbers)
+                .flatMap(Employee -> Employee.getEmpPhoneNumbers().stream())
+                .forEach(System.out::println);
+
+        System.out.println("Print all phone numbers with Double Colon");
+
+        EmployeeData.readAll()
+                .map(Employee::getEmpPhoneNumbers)
+                .flatMap(List::stream)
+                .forEach(System.out::println);
 
     }
 }
