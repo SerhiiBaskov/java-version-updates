ce344866fae00d28d728d17accbf91da9629c6f1
Sergiy Baskov <master.flock@gmail.com>
2023-05-30 22:04:43 +0300

    Reducing

--- m6-stream/src/main/java/com/cydeo/tasks/Reducing.java
+++ m6-stream/src/main/java/com/cydeo/tasks/Reducing.java
@@ -5,6 +5,7 @@ import com.cydeo.tasks.dishTask.DishData;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Reducing {
     public static void main(String[] args) {
@@ -18,11 +19,26 @@ public class Reducing {
 
         int result = numbers.stream().reduce(0,(a,b) -> a+b);
         System.out.println(result);
+        Optional<Integer> result2 = numbers.stream().reduce((a, b) -> a+b);
+        System.out.println(result2.get());
 
         System.out.println("Total calories of menu");
 
-        int sumCalories = DishData.getAll().stream().map(Dish::getCalories) .reduce(0, (a, b) -> a+b);
+        int sumCalories = DishData.getAll().stream()
+                .map(Dish::getCalories)
+ //             .reduce(0, (a,b) -> a+b);
+                .reduce(Integer::sum).get();
         System.out.println(sumCalories);
+
+        //Max and min
+
+        Optional<Integer> max = numbers.stream().reduce(Integer::max);
+        Optional<Integer> min = numbers.stream().reduce(Integer::min);
+        Optional<Integer> sum = numbers.stream().reduce(Integer::sum);
+
+        System.out.println("Min: " +min.get());
+        System.out.println("Max: " +max.get());
+        System.out.println("Sum: " +sum.get());
 
 
 
