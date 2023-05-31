9e7340df076a171a216f96fe4d88c7ed84626118
Sergiy Baskov <master.flock@gmail.com>
2023-05-31 14:18:30 +0300

    Collectors

--- m6-stream/src/main/java/com/cydeo/CollectorsDemo.java
+++ m6-stream/src/main/java/com/cydeo/CollectorsDemo.java
@@ -60,6 +60,20 @@ public class CollectorsDemo {
 
         System.out.println(evenCount);
 
+        // averagingInt(ToIntFunction) - returns the average of the integers passed values
+
+        Double calorieAverage = DishData.getAll().stream()
+                .collect(Collectors.averagingInt(Dish::getCalories));
+
+        System.out.println(calorieAverage);
+
+        // joining() - is used to join various elements of a character or string array into a single object
+
+        List<String> courses = Arrays.asList("Java", "JS", "TS");
+        String str = courses.stream()
+                .collect(Collectors.joining(","));
+
+        System.out.println(str);
 
 
     }
