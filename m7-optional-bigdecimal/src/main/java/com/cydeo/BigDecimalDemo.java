acded7c71c82dadc54a40d9e8f730f49a82f9d6c
Sergiy Baskov <master.flock@gmail.com>
2023-06-19 20:57:02 +0300

    Factory Methods

--- m7-optional-bigdecimal/src/main/java/com/cydeo/BigDecimalDemo.java
+++ m7-optional-bigdecimal/src/main/java/com/cydeo/BigDecimalDemo.java
@@ -1,6 +1,7 @@
 package com.cydeo;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class BigDecimalDemo {
     public static void main(String[] args) {
@@ -19,6 +20,20 @@ public class BigDecimalDemo {
         System.out.println(b1.add(BigDecimal.TEN));
         System.out.println(b1.multiply(new BigDecimal("15.1")));
         System.out.println(b1.multiply(BigDecimal.valueOf(15.56)));
+
+        // Scaling
+
+        BigDecimal number1 = new BigDecimal("23.12");
+        System.out.println(number1.setScale(1, RoundingMode.CEILING));
+        System.out.println(number1.setScale(3, RoundingMode.CEILING));
+        System.out.println(number1.setScale(1, RoundingMode.FLOOR));
+        System.out.println(number1.setScale(3, RoundingMode.FLOOR));
+
+        // Do not use equals(). Use compareTo()
+
+        System.out.println(new BigDecimal("2").compareTo(BigDecimal.valueOf(2)));
+        System.out.println(new BigDecimal("3").compareTo(BigDecimal.valueOf(2)));
+        System.out.println(new BigDecimal("1").compareTo(BigDecimal.valueOf(2)));
 
     }
 }
